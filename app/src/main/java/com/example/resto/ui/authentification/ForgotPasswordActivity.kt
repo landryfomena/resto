a21eef626bac39bc51f575b6d91fd452f8d482e7
Oldrich <oldrichngnitedem4@gmail.com>
2021-02-24 18:49:20 +0100

    forgotpassword

---
+++ app/src/main/java/com/example/resto/ui/authentification/ForgotPasswordActivity.kt
@@ -0,0 +1,22 @@
+package com.example.resto.ui.authentification
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.example.resto.MainActivity
+import com.example.resto.R
+
+class ForgotPasswordActivity : AppCompatActivity() {
+    lateinit var send: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password)
+        send=findViewById<Button>(R.id.send_btn)
+        send.setOnClickListener {
+            val intent= Intent(this@ForgotPasswordActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
