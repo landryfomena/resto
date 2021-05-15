8c02d78970b05ae3df22e52f8a5c5d464ef80757
Noe Kenfack <kenfaclnoe@gmail.com>
2021-05-15 13:19:13 +0100

    New Design

---
+++ app/src/main/java/com/example/resto/AddNewMenu.kt
@@ -0,0 +1,31 @@
+package com.example.resto
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddNewMenu : Fragment() {
+
+    companion object {
+        fun newInstance() = AddNewMenu()
+    }
+
+    private lateinit var viewModel: AddNewMenuViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return inflater.inflate(R.layout.add_new_menu_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AddNewMenuViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/resto/AddNewMenuViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.resto
+
+import androidx.lifecycle.ViewModel
+
+class AddNewMenuViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
